5eb09cb9b34a30dc6cfaad89f04541e6eace6d34
Igor V <igorvv.vn@gmail.com>
2023-02-05 13:34:33 +0300

    initial

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<!-- eslint-disable no-unused-vars -->
+<script>
+import CardContainer from "./components/CardContainer.vue";
+import { WEATHER_DATA } from "./mockData";
+
+const API_KEY = "09a763398032247c5ca506d40b4fd6ea";
+
+export default {
+  data() {
+    return {
+      json: WEATHER_DATA
+  }
+ },
+ methods: {
+    fetchData () {
+      
+    },
+    fetchCity () {
+
+    },
+ },
+ components: {
+  CardContainer
+ },
+}
+</script>
+
+<template>
+  <div class="container">
+    <CardContainer :data="json"></CardContainer>
+  </div>
+</template>
+
+<style scoped></style>
